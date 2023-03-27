0c5799db3fbc18d124e67c322d37aa44c13de435
wangjichao <wjc163@sina.cn>
2023-03-27 20:35:14 +0800

    feat: add prototype mode

--- create/prototype/prototype.go
+++ create/prototype/prototype.go
@@ -1,8 +1,7 @@
-package main
+package prototype
 
 import (
 	"encoding/json"
-	"testing"
 	"time"
 )
 
@@ -43,43 +42,4 @@ func (words *Keywords) Clone(updateWords []*Keyword) Keywords {
 	}
 
 	return newKeywords
-}
-
-// 给上面代码生成测试用例
-func TestKeywords_Clone(t *testing.T) {
-	// 初始化数据
-	words := Keywords{
-		"hello": &Keyword{
-			word:  "hello",
-			visit: 100,
-		},
-		"world": &Keyword{
-			word:  "world",
-			visit: 200,
-		},
-	}
-
-	// 生成更新数据
-	updateWords := []*Keyword{
-		&Keyword{
-			word:  "hello",
-			visit: 300,
-		},
-		&Keyword{
-			word:  "world",
-			visit: 400,
-		},
-	}
-
-	// 拷贝数据
-	newWords := words.Clone(updateWords)
-
-	// 检查是否拷贝成功
-	if newWords["hello"].visit != 300 {
-		t.Error("拷贝数据失败")
-	}
-
-	if newWords["world"].visit != 400 {
-		t.Error("拷贝数据失败")
-	}
 }
